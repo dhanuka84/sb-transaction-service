e3d4f6e2960f61c25e0b9aeee781caa7e051e21a
dhanuka84 <dhanuka.priyanath@gmail.com>
2021-08-21 21:29:51 +0800

    code changes

--- src/main/java/com/sb/TransactionApplication.java
+++ src/main/java/com/sb/TransactionApplication.java
@@ -3,8 +3,6 @@ package com.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 //@EnableFeignClients //needs to be on SBA class
 @SpringBootApplication
